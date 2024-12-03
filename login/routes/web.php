46272bf091701e943164bfa5ce2f339c1d5f981b
Al Mahbub <almahbub25@gmail.com>
2024-12-03 22:01:02 +0600

    user seeder has been created

--- login/routes/web.php
+++ login/routes/web.php
@@ -4,17 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 
 
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
 Route::get('/', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-// Protected route example
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware('auth');
